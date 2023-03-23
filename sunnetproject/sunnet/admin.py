054753bdf6e3ab8a7d98228430a9e26b75d4a670
gonenbilgiislem <bilgiislem@balikesirgonen.bel.tr>
2023-03-23 08:26:40 +0300

    ..

--- sunnetproject/sunnet/admin.py
+++ sunnetproject/sunnet/admin.py
@@ -3,19 +3,9 @@ from .models import GELEN_TAKILAR, KISILER, TAKI_TURU
 
 # Register your models here.
 class GELEN_TAKILARAdmin(admin.ModelAdmin):
-    list_display = ('ID', 'AD', 'SOYAD', 'ACIKLAMA', 'MIKTAR', 'TAKI_TUR_ID', 'KISI_ID')
-    list_filter = ('ID', 'AD', 'SOYAD', 'ACIKLAMA', 'MIKTAR', 'TAKI_TUR_ID', 'KISI_ID')
-
-class KISILERAdmin(admin.ModelAdmin):
-    list_display = ('ID', 'AD')
-    list_filter = ('ID', 'AD')
-
-class TAKI_TURUAdmin(admin.ModelAdmin):
-    list_display = ('ID', 'AD')
-    list_filter = ('ID', 'AD')
-
-
+    list_display = ('AD', 'SOYAD', 'ACIKLAMA', 'MIKTAR')
+    list_filter = ('AD', 'SOYAD', 'ACIKLAMA', 'MIKTAR')
 
 admin.site.register(GELEN_TAKILAR, GELEN_TAKILARAdmin)
-admin.site.register(KISILER, KISILERAdmin)
-admin.site.register(TAKI_TURU, TAKI_TURUAdmin)
+admin.site.register(KISILER)
+admin.site.register(TAKI_TURU)
--- sunnetproject/sunnet/models.py
+++ sunnetproject/sunnet/models.py
@@ -8,18 +8,21 @@
 from django.db import models
 
 class GELEN_TAKILAR(models.Model):
-    ID = models.AutoField(primary_key=True)
+    # ID = models.AutoField(primary_key=True)
     AD = models.TextField(db_column='Ad')  # Field name made lowercase.
     SOYAD = models.TextField(db_column='Soyad', blank=True, null=True)  # Field name made lowercase.
     ACIKLAMA = models.TextField(blank=True, null=True)
     MIKTAR = models.IntegerField(blank=True, null=True)
-    TAKI_TUR_ID = models.ForeignKey('TAKI_TURU', models.SET_NULL, blank=True, null=True,db_column = 'TAKI_TUR_ID')
-    KISI_ID = models.ForeignKey('KISILER', models.SET_NULL, blank=True, null=True,db_column = 'KISI_ID')
+    TAKI_TUR_ID = models.ForeignKey('TAKI_TURU', models.SET_NULL, blank=True, null=True)
+    KISI_ID = models.ForeignKey('KISILER', models.SET_NULL, blank=True, null=True)
 
 class KISILER(models.Model):
-    ID = models.AutoField(primary_key=True)
+    def __str__(self):
+            return self.AD
     AD = models.TextField(blank=True, null=True)
 
 class TAKI_TURU(models.Model):
-    ID = models.AutoField(primary_key=True)
+    # ID = models.AutoField(primary_key=True)
+    def __str__(self):
+            return self.AD
     AD = models.TextField(blank=True, null=True)
