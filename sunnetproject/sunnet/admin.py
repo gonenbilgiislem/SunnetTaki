1994abe8ddd8f5ed290ec222240abc1841c516de
gonenbilgiislem <bilgiislem@balikesirgonen.bel.tr>
2023-03-27 02:04:42 +0300

    ..

--- sunnetproject/sunnet/admin.py
+++ sunnetproject/sunnet/admin.py
@@ -9,20 +9,22 @@ from django.utils.html import format_html
 # Register your models here.
 
 
-@admin.display(description='Adı Soyadı')
+@admin.display(description='Adı Soyadı', ordering='AD')
 def upper_case_name(obj):
-    return ("%s %s" % (obj.AD, obj.SOYAD)).upper()
+    # return ("%s %s" % (obj.AD, obj.SOYAD)).upper()
+    return f'{obj.AD} {obj.SOYAD.upper()}'
 
 
 @admin.display(description='MIKTAR')
 def float_miktar(obj):
     return format_html('<b>{}</b>', floatformat(obj.MIKTAR, -2))
 
+
 class GELEN_TAKILAR_Admin(admin.ModelAdmin):
     list_display = ("id",upper_case_name, 'ACIKLAMA', float_miktar,'KISI','TAKI_TUR')
     list_filter = ('TAKI_TUR', 'KISI','MIKTAR')
     search_fields = ("id", 'AD', 'SOYAD', 'ACIKLAMA', 'MIKTAR')
-    ordering = ('id', 'AD', 'SOYAD', 'ACIKLAMA', 'MIKTAR')
+    ordering = ('id',  'ACIKLAMA', 'MIKTAR', 'TAKI_TUR', 'KISI')
     list_per_page = 10
     list_display_links = (upper_case_name,)
     # list_editable = ('MIKTAR','KISI','TAKI_TUR')
